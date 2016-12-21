b0af2cc548c5a305e2e176ece36726df378ed316
Nature <liaoqing24@aliyun.com>
2016-12-21 02:08:58 -0800

    v0.12.21 Create new function and learn tensorflow.

--- demo.py
+++ demo.py
@@ -1,6 +1,8 @@
+import numpy as np
 import tensorflow as tf
 
 from newfunc import nnn
+sess = tf.InteractiveSession()
 
 # Create a Constant op that produces a 1x2 matrix.  The op is
 # added as a node to the default graph.
@@ -8,17 +10,21 @@ from newfunc import nnn
 # The value returned by the constructor represents the output
 # of the Constant op.
 matrix1 = tf.constant([[3., 3.]])
+print sess.run(matrix1)
 
 # Create another Constant that produces a 2x1 matrix.
 matrix2 = tf.constant([[2.], [2.]])
+print sess.run(matrix2)
 
 # Create a Matmul op that takes 'matrix1' and 'matrix2' as inputs.
 # The returned value, 'product', represents the result of the matrix
 # multiplication.
-product = tf.matmul(matrix1, matrix2)
+product = sess.run(tf.matmul(matrix1, matrix2))
 
 print product
 
+x = tf.ones([2, 3], tf.int32)
+print(sess.run(x))
 
 def abc(a):
     print a
@@ -26,3 +32,32 @@ def abc(a):
 
 abc("hello")
 print nnn(10)
+
+
+test = tf.constant([1, 2, 3])
+test2 = tf.constant(-1.0, shape=[2, 3])
+print sess.run(test)
+print sess.run(test2)
+
+print(sess.run(tf.fill([2,3],2)))
+print(sess.run(tf.constant(2,shape=[2,3])))
+
+t=tf.sparse_to_dense([[1,1],[2,1],[2,4],[4,5]],[10,10],[1,2,4,5])
+print(sess.run(t))
+print(sess.run([test,test2]))
+a = tf.Variable([0.0,1.0])
+b = tf.placeholder(dtype=tf.float32,shape=[2])
+op = tf.assign(a,b)
+sess.run(tf.global_variables_initializer())
+print(sess.run(a))
+sess.run(op,feed_dict={b:[5.,1.]})
+print(sess.run(a))
+mylist=['sd']
+assert len(mylist) >= 1
+mylist.pop()
+mylist
+
+with tf.variable_scope("foo"):
+    with tf.variable_scope("bar"):
+        v = tf.get_variable("v", [1])
+        assert v.name == "foo/bar/v:0"
---
+++ demo2.py
@@ -0,0 +1,39 @@
+import tensorflow as tf
+
+sess = tf.InteractiveSession()
+
+
+def weight_variable(shape):
+    initial = tf.truncated_normal(shape, stddev=0.1)
+    return tf.Variable(initial)
+
+
+def bias_variable(shape):
+    initial = tf.constant(0.1, shape=shape)
+    return tf.Variable(initial)
+
+
+def conv2d(x, W):
+    return tf.nn.conv2d(x, W, strides=[1, 1, 1, 1], padding='SAME')
+
+
+def max_pool_2x2(x):
+    return tf.nn.max_pool(x, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
+
+
+W_conv1 = weight_variable([5, 5, 1, 32])
+b_conv1 = bias_variable([32])
+
+x_image = tf.reshape(x, [-1,28,28,1])
+
+h_conv1 = tf.nn.relu(conv2d(x_image, W_conv1) + b_conv1)
+h_pool1 = max_pool_2x2(h_conv1)
+
+W_conv2 = weight_variable([5, 5, 32, 64])
+b_conv2 = bias_variable([64])
+
+h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2)
+h_pool2 = max_pool_2x2(h_conv2)
+
+sess.run(tf.global_variables_initializer())
+print (sess.run(W_conv1))
